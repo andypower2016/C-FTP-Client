c0cbbf78113c95e82792f95d5de9f94bb32e404e
andypower2016 <73726447+andypower2016@users.noreply.github.com>
2020-10-31 16:22:15 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace FTPClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.Connect = new System.Windows.Forms.Button();
+            this.buttonUploadFile = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.labelCurrentFile = new System.Windows.Forms.Label();
+            this.buttonDownload = new System.Windows.Forms.Button();
+            this.progressBarDownload = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(7, 10);
+            this.textBoxIP.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(109, 22);
+            this.textBoxIP.TabIndex = 0;
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(119, 10);
+            this.textBoxPort.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(47, 22);
+            this.textBoxPort.TabIndex = 1;
+            // 
+            // Connect
+            // 
+            this.Connect.Location = new System.Drawing.Point(170, 10);
+            this.Connect.Margin = new System.Windows.Forms.Padding(2);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(71, 20);
+            this.Connect.TabIndex = 2;
+            this.Connect.Text = "Connect";
+            this.Connect.UseVisualStyleBackColor = true;
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // buttonUploadFile
+            // 
+            this.buttonUploadFile.Location = new System.Drawing.Point(170, 44);
+            this.buttonUploadFile.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonUploadFile.Name = "buttonUploadFile";
+            this.buttonUploadFile.Size = new System.Drawing.Size(71, 22);
+            this.buttonUploadFile.TabIndex = 3;
+            this.buttonUploadFile.Text = "UploadFile";
+            this.buttonUploadFile.UseVisualStyleBackColor = true;
+            this.buttonUploadFile.Click += new System.EventHandler(this.buttonUploadFile_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(7, 44);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(2);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(158, 22);
+            this.progressBar.TabIndex = 4;
+            // 
+            // labelCurrentFile
+            // 
+            this.labelCurrentFile.AutoSize = true;
+            this.labelCurrentFile.Location = new System.Drawing.Point(9, 198);
+            this.labelCurrentFile.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelCurrentFile.Name = "labelCurrentFile";
+            this.labelCurrentFile.Size = new System.Drawing.Size(0, 12);
+            this.labelCurrentFile.TabIndex = 5;
+            // 
+            // buttonDownload
+            // 
+            this.buttonDownload.Location = new System.Drawing.Point(170, 75);
+            this.buttonDownload.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonDownload.Name = "buttonDownload";
+            this.buttonDownload.Size = new System.Drawing.Size(71, 22);
+            this.buttonDownload.TabIndex = 6;
+            this.buttonDownload.Text = "Download";
+            this.buttonDownload.UseVisualStyleBackColor = true;
+            this.buttonDownload.Click += new System.EventHandler(this.buttonDownload_Click);
+            // 
+            // progressBarDownload
+            // 
+            this.progressBarDownload.Location = new System.Drawing.Point(7, 75);
+            this.progressBarDownload.Margin = new System.Windows.Forms.Padding(2);
+            this.progressBarDownload.Name = "progressBarDownload";
+            this.progressBarDownload.Size = new System.Drawing.Size(158, 22);
+            this.progressBarDownload.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 252);
+            this.Controls.Add(this.progressBarDownload);
+            this.Controls.Add(this.buttonDownload);
+            this.Controls.Add(this.labelCurrentFile);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.buttonUploadFile);
+            this.Controls.Add(this.Connect);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.textBoxIP);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "FTPClient";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.Button buttonUploadFile;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label labelCurrentFile;
+        private System.Windows.Forms.Button buttonDownload;
+        private System.Windows.Forms.ProgressBar progressBarDownload;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace FTPClient
+{
+    public partial class Form1 : Form
+    {
+
+        static class CommandDefine
+        {
+            public const string ctransferStart = "501";
+            public const string ctransfering = "502";
+            public const string ctransferEnd = "503";
+
+            public const string stransferStart = "601";
+            public const string stransfering = "602";
+            public const string stransferEnd = "603";
+
+            public const byte packetHead = 0x000;
+            public const byte dataHead = 0x001;
+        }
+
+        TcpClient tcpClient;
+        OpenFileDialog openFileDlg;   
+        
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            openFileDlg = new OpenFileDialog();
+            openFileDlg.Multiselect = true;
+
+            textBoxIP.Text = "219.87.85.162";//GetLocalIPAddress();
+            textBoxPort.Text = "10025";
+        }
+
+        private void Connect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tcpClient = new TcpClient(textBoxIP.Text, Convert.ToInt32(textBoxPort.Text));
+                //textBoxPort.Text = "192.168.165.170";
+                //tcpClient = new TcpClient(); 
+                //IPAddress ipAddress = Dns.GetHostEntry(textBoxPort.Text).AddressList[0];
+                //IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 100);
+                //tcpClient.Connect(ipEndPoint);
+            }
+            catch (SocketException socketException)
+            {
+                string message;
+                message = String.Format("Connect Fail, errorCode={0}", socketException.ErrorCode);
+                MessageBox.Show(message);
+            } 
+        }
+
+        private void buttonUploadFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                int maxBufferLen = 50000;
+                // Read the files
+                foreach (String file in openFileDlg.FileNames)
+                {
+                    labelCurrentFile.Text = "Current transfering file : " + file;        
+                    Boolean completeFlag = false;        
+                    NetworkStream networkStream = tcpClient.GetStream();
+                    FileStream fstream = new FileStream(file, FileMode.Open);                
+                    byte[] fileNameData = CreateDataPacket(Encoding.UTF8.GetBytes(CommandDefine.ctransferStart), Encoding.UTF8.GetBytes(Path.GetFileName(file)));  
+                    networkStream.Write(fileNameData, 0, fileNameData.Length);
+                    networkStream.Flush();
+                  
+                    while (true)
+                    {
+                        if (networkStream.ReadByte() == CommandDefine.packetHead)
+                        {
+                            byte[] command = new byte[3];
+                            networkStream.Read(command, 0, command.Length);                         
+                            byte[] recvData = ReadDataPacket(networkStream);
+                            switch (System.Text.Encoding.UTF8.GetString(command))
+                            {
+                                case CommandDefine.ctransfering:
+                                    long curFilePointer = long.Parse(Encoding.UTF8.GetString(recvData));    // receive file pointer from server
+                                    if (curFilePointer != fstream.Length && curFilePointer < fstream.Length)
+                                    {
+                                        fstream.Seek(curFilePointer, SeekOrigin.Begin);
+                                        int sendBufferLen = (int)( (fstream.Length - curFilePointer) < maxBufferLen ? (fstream.Length - curFilePointer) : maxBufferLen );
+                                        byte[] sendBuffer = new byte[sendBufferLen];
+                                        fstream.Read(sendBuffer, 0, sendBuffer.Length);
+                                        byte[] sendData = CreateDataPacket(Encoding.UTF8.GetBytes(CommandDefine.ctransfering), sendBuffer);  // send file data to Server
+                                        networkStream.Write(sendData, 0, sendData.Length);
+                                        networkStream.Flush();
+                                        UpdateProgressBar(curFilePointer, fstream.Length);
+                                    }
+                                    else 
+                                    {
+                                        byte[] sendData = CreateDataPacket(Encoding.UTF8.GetBytes(CommandDefine.ctransferEnd), Encoding.UTF8.GetBytes("Close"));    // file transfer complete, send Close message
+                                        networkStream.Write(sendData, 0, sendData.Length);
+                                        networkStream.Flush();
+                                        UpdateProgressBar(curFilePointer, fstream.Length);
+                                        fstream.Close();
+                                        completeFlag = true;
+                                    }
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        if (completeFlag == true)
+                        {
+                            networkStream.Flush();
+                            labelCurrentFile.Text = @"";
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void buttonDownload_Click(object sender, EventArgs e)
+        {
+            string downloadFolder = @"D:\Me\Code\C#\FTP Server Code\download folder";
+            openFileDlg.InitialDirectory = @"\\" + textBoxIP.Text + @"\D$";
+            if (openFileDlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                NetworkStream networkStream = tcpClient.GetStream();
+                FileStream fstream = null;
+
+                foreach (String filePath in openFileDlg.FileNames)
+                {
+                    Boolean completeFlag = false;
+                    long curfilePointer = 0;
+                    string fileName = Path.GetFileName(filePath);
+                    string downloadFilePath = downloadFolder + '\\' + fileName;
+                    long fileLength = new System.IO.FileInfo(filePath).Length;
+                    fstream = new FileStream(downloadFilePath, FileMode.Create);
+                    byte[] filePathData = CreateDataPacket(Encoding.UTF8.GetBytes(CommandDefine.stransferStart), Encoding.UTF8.GetBytes(filePath));   // Send filePath to Server
+                    networkStream.Write(filePathData, 0, filePathData.Length);
+                    networkStream.Flush();
+                    while (true)
+                    {
+                        if (networkStream.ReadByte() == CommandDefine.packetHead)
+                        {
+                            byte[] command = new byte[3];
+                            networkStream.Read(command, 0, command.Length);
+                            byte[] receivedData = ReadDataPacket(networkStream);
+                            switch (System.Text.Encoding.UTF8.GetString(command))
+                            {
+                                case CommandDefine.stransfering:
+                                    {
+                                        fstream.Seek(curfilePointer, SeekOrigin.Begin);
+                                        fstream.Write(receivedData, 0, receivedData.Length);
+                                        curfilePointer = fstream.Position;
+                                        byte[] sendData = CreateDataPacket(System.Text.Encoding.UTF8.GetBytes(CommandDefine.stransfering), System.Text.Encoding.UTF8.GetBytes(System.Convert.ToString(curfilePointer)));
+                                        networkStream.Write(sendData, 0, sendData.Length);
+                                        networkStream.Flush();
+                                        UpdateProgressBarDownload(curfilePointer, fileLength);
+                                    }
+                                    break;
+                                case CommandDefine.stransferEnd:
+                                    {
+                                        UpdateProgressBarDownload(curfilePointer, fileLength);
+                                        curfilePointer = 0;
+                                        networkStream.Flush();
+                                        fstream.Close();
+                                        completeFlag = true;
+                                    }
+                                    break;
+
+                                default:
+                                    break;
+                            }
+                            if (completeFlag)
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void UpdateProgressBar(long progress, long total)
+        {
+            progressBar.Value = (int)Math.Ceiling((double)progress * 100 / (double)total);
+            if (progressBar.Value == 100)
+            {
+                progressBar.Value = 0;
+            }
+        }
+
+        public void UpdateProgressBarDownload(long progress, long total)
+        {
+            progressBarDownload.Value = (int)Math.Ceiling((double)progress * 100 / (double)total);
+            if (progressBarDownload.Value == 100)
+            {
+                progressBarDownload.Value = 0;
+            }
+        }
+
+        
+        private byte[] ReadDataPacket(NetworkStream ns)
+        {
+            byte[] dataPacket = null;
+            int length = 0;
+            String strDataLength = "";
+            while ((length = ns.ReadByte()) != CommandDefine.dataHead) // read data length
+            {
+                strDataLength += (char)length;
+            }
+            int dataLength = System.Convert.ToInt32(strDataLength);
+            dataPacket = new byte[dataLength];
+
+            int byteOffset = 0;
+            while (byteOffset < dataLength)
+            {
+                byteOffset += ns.Read(dataPacket, byteOffset, dataLength - byteOffset);
+            }
+            return dataPacket;
+        }
+
+        private byte[] CreateDataPacket(byte[] cmd, byte[] data)
+        {
+            byte[] pacHead = new byte[1];
+            pacHead[0] = CommandDefine.packetHead;
+            byte[] datHead = new byte[1];
+            datHead[0] = CommandDefine.dataHead;
+            byte[] datalength = System.Text.Encoding.UTF8.GetBytes(System.Convert.ToString(data.Length));
+            MemoryStream stream = new MemoryStream();
+            stream.Write(pacHead, 0, pacHead.Length);
+            stream.Write(cmd, 0, cmd.Length);
+            stream.Write(datalength, 0, datalength.Length);
+            stream.Write(datHead, 0, datHead.Length);
+            stream.Write(data, 0, data.Length);
+            return stream.ToArray();
+        }
+
+        
+    }
+}
